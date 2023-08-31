93612775840c070b2a48ccc189434661e038b429
nitesh sharma <nitesh.sharma@deliveryhero.com>
2023-08-31 22:22:48 +0200

    Deploy

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -1,42 +1,46 @@
-'use client'
-import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { CalendarIcon, PaperClipIcon, TagIcon, UserCircleIcon } from '@heroicons/react/20/solid'
+"use client";
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import {
+  CalendarIcon,
+  PaperClipIcon,
+  TagIcon,
+  UserCircleIcon,
+} from "@heroicons/react/20/solid";
 
 const assignees = [
-  { name: 'Unassigned', value: null },
+  { name: "Unassigned", value: null },
   {
-    name: 'Wade Cooper',
-    value: 'wade-cooper',
+    name: "Wade Cooper",
+    value: "wade-cooper",
     avatar:
-      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   // More items...
-]
+];
 const labels = [
-  { name: 'Unlabelled', value: null },
-  { name: 'Engineering', value: 'engineering' },
+  { name: "Unlabelled", value: null },
+  { name: "Engineering", value: "engineering" },
   // More items...
-]
+];
 const dueDates = [
-  { name: 'No due date', value: null },
-  { name: 'Today', value: 'today' },
+  { name: "No due date", value: null },
+  { name: "Today", value: "today" },
   // More items...
-]
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
-export default function Form({setResponse}: any): JSX.Element {
- const [text, setText] = useState("")
+export default function Form({ setResponse }: any): JSX.Element {
+  const [text, setText] = useState("");
 
   return (
     <form className="relative">
       <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
-        
         <label htmlFor="description" className="sr-only">
-          Paste the text to extract links from 
+          Paste the text to extract links from
         </label>
         <textarea
           rows={20}
@@ -44,11 +48,11 @@ export default function Form({setResponse}: any): JSX.Element {
           id="description"
           className="block w-full resize-none border-0 py-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           placeholder="Paste the text to extract links from"
-          defaultValue={''}
+          defaultValue={""}
           maxLength={1000}
           onChange={(e: any) => {
-            e.preventDefault()
-            setText(e.currentTarget.value)
+            e.preventDefault();
+            setText(e.currentTarget.value);
           }}
         />
 
@@ -69,30 +73,30 @@ export default function Form({setResponse}: any): JSX.Element {
       <div className="absolute inset-x-px bottom-0">
         {/* Actions: These are just examples to demonstrate the concept, replace/wire these up however makes sense for your project. */}
         <div className="flex flex-nowrap justify-end space-x-2 px-2 py-2 sm:px-3">
-        <div className="flex-shrink-0">
+          <div className="flex-shrink-0">
             <button
               type="submit"
               className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               onClick={async (e: any) => {
-                e.preventDefault()
-                const response = await fetch('/api', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({prompt: text})
-                 })
-                 
-                 const json = await response.text()
-                 console.log(json)
-                 setResponse(json)
+                e.preventDefault();
+                const response = await fetch("/api", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({ prompt: text }),
+                });
+
+                const json = await response.text();
+                console.log(json);
+                setResponse(json);
               }}
             >
               Get the links
             </button>
           </div>
-          </div>
+        </div>
       </div>
     </form>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,35 @@
-'use client'
-import Image from 'next/image'
-import Form from './components/form'
-import { useState } from 'react'
-
+"use client";
+import Image from "next/image";
+import Form from "./components/form";
+import { useState } from "react";
 
 export default function Home() {
-  const [response, setResponse] = useState("")
-  
-  
+  const [response, setResponse] = useState("");
+
   return (
     <>
-      
-      
       <div className="mx-auto max-w-2xl lg:text-center mt-10 mb-20">
-            
-            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Extract links from zoom chat.
-            </p>
-            
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Extract links from zoom chat.
+        </p>
+      </div>
+      <div className="flex justify-around">
+        <div className="w-1/2 p-4">
+          <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
+            <Form setResponse={setResponse} />
           </div>
-      
-        
-        <main className="lg:pl-72 flex">
-          <div className="xl:pl-96 flex-grow">
-            <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
-              <Form setResponse={setResponse}/>
-            </div>
-          </div>
-          </main>
-          <div className='brd-1'>
-            {response}
-          </div>
-        
-        
+        </div>
+        <div className="w-1/3 p-10">{response}</div>
+      </div>
 
-       
-        
-        
-      
+      {/* <main className="lg:pl-72 flex">
+        <div className="xl:pl-96 flex-grow">
+          <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
+            <Form setResponse={setResponse} />
+          </div>
+        </div>
+      </main>
+      <div className="border border-solid border-red-500">{response}</div> */}
     </>
-  )
+  );
 }
